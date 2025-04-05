bf9a73f360b363d75a2595608004dc5f121275e0
Parya <parya3252@gamil.com>
2025-04-05 12:50:22 +0530

    addfile1

---
+++ 01_Basics/3.py
@@ -0,0 +1,22 @@
+name = "Prince"
+age = 20
+wight =50.50
+
+print(type(name))
+print(type(age))
+print(type(wight))
+  
+
+#   data types
+# int 
+# String
+# float
+# boolean
+# none
+
+age = 20 
+old = False
+a = None
+
+print(type(old))
+print(type(a))
